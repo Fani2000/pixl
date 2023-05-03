da02a945a82bd75b3f0f9e99875d94e000ece59d
Fani <keorapetsefani2000@gmail.com>
2023-05-03 23:16:14 +0200

    Added the functionality and logic for the swatch of the app

---
+++ swatch/swatch.go
@@ -0,0 +1,45 @@
+package swatch
+
+import (
+	"fani2000/apptype"
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/widget"
+)
+
+type Swatch struct {
+	widget.BaseWidget
+	Selected     bool
+	Color        color.Color
+	SwatchIndex  int
+	clickHandler func(s *Swatch)
+}
+
+func (s *Swatch) setColor(c color.Color) {
+	s.Color = c
+	s.Refresh()
+}
+
+func NewSwatch(state *apptype.State, color color.Color, swatchIndex int, clickHandler func(s *Swatch)) *Swatch {
+	swatch := &Swatch{
+		Selected:     false,
+		Color:        color,
+		clickHandler: clickHandler,
+		SwatchIndex:  swatchIndex,
+	}
+	swatch.ExtendBaseWidget(swatch)
+
+	return swatch
+}
+
+func (swatch *Swatch) CreateRenderer() fyne.WidgetRenderer {
+	square := canvas.NewRectangle(swatch.Color)
+	objects := []fyne.CanvasObject{square}
+	return &SwatchRenderer{
+		square:  *square,
+		objects: objects,
+		parent:  swatch,
+	}
+}
