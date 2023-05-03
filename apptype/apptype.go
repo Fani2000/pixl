f4dd134ef1a5eed41b62e666235ee6d5e2576e80
Fani <keorapetsefani2000@gmail.com>
2023-05-03 22:50:57 +0200

    Initial apptypes created

---
+++ apptype/apptype.go
@@ -0,0 +1,28 @@
+package apptype
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+)
+
+// Types
+type BrushType = int
+
+type PxCanvasConfig struct {
+	DrawingArea    fyne.Size
+	CanvasOffset   fyne.Position
+	PxRows, PxCols int
+	PxSize         int
+}
+
+type State struct {
+	BrushColor     color.Color
+	BrushType      int
+	SwatchSelected int
+	FilePath       string
+}
+
+func (state *State) setFilePath(path string) {
+	state.FilePath = path
+}
